90e07703ffe178b0b79fcfb54008c22cc4403565
Rohan Tejeswar <roh123098@gmail.com>
2021-02-03 00:13:24 +0530

    added simple styles

--- src/components/LoadoutInput.js
+++ src/components/LoadoutInput.js
@@ -4,14 +4,31 @@ class LoadoutInput extends React.Component {
   constructor() {
     super()
 
-    this.state = {}
+    this.state = {
+      loadoutCode: "",
+      color: ""
+    }
   }
 
   render() {
     return (
-      <div uk-alert>
-        <div>
-          close
+      <div className="uk-alert">
+        <div className="uk-flex-inline uk-width-expand">
+          <div className="uk-width-2-3">
+            <div className="uk-background-default uk-margin-small-right uk-padding-small uk-flex">
+              <label className="uk-text-bold">Code</label>
+            </div>
+          </div>
+
+          <div className="uk-width-1-3">
+            <div className="uk-background-default uk-margin-small-right uk-padding-small uk-flex">
+              <label className="uk-text-bold">Color</label>
+            </div>
+          </div>
+
+          <div className="uk-width-auto uk-margin-auto-left uk-flex uk-flex-center uk-flex-middle">
+            <span uk-icon="icon: close; ratio: 1"></span>
+          </div>
         </div>
       </div>
     )
