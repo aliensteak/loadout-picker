fd34eb75927ff0075f0fff6a5cbcf31f4599bf14
Rohan Tejeswar <roh123098@gmail.com>
2021-02-14 18:38:24 +0530

    loadoutSqf is now generated

--- src/components/GenerateCode.js
+++ src/components/GenerateCode.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 
-import sampleLoadoutUrl from "./sampleSqf.sqf"
 class GenerateCode extends Component {
   constructor(props) {
     super(props)
@@ -20,64 +19,98 @@ class GenerateCode extends Component {
     return string
   }
 
+  getProcessedCode(loadoutCode, roleName) {
+    // split code based on blocks of code
+    let loadout = loadoutCode.split("\n\n")
+
+    // if code is copied from google docs/ words then do this
+    if (loadout.length === 1) loadout = loadoutCode.split("\r\n\r\n")
+
+    // index 0; loadout author comment
+    const block0 = loadout[0]
+    const loadoutAuthor = encodeURI(
+      block0
+        .replace(/(comment "Exported from Arsenal by )/g, "")
+        .replace(/(";)/g, "")
+    )
+
+    // index 1; unit local; ignore this block
+
+    // index 2; remove all gear
+    const block1 = this.addTabs(loadout[2]).replaceAll("this", "player")
+
+    // index 3; add weapons
+    const block2 = this.addTabs(loadout[3]).replaceAll("this", "player")
+
+    // index 4; add containers
+    const block3 = this.addTabs(loadout[4]).replaceAll("this", "player")
+
+    // index 5; add items in containers
+    const block4 = this.addTabs(loadout[5]).replaceAll("this", "player")
+
+    // index 6; add items
+    const block5 = this.addTabs(loadout[6]).replaceAll("this", "player")
+
+    // index 7; set identity; ignore this block
+
+    // add all blocks to a loadout string
+    let loadoutContainer =
+      '\t\tcomment "Loadout for ' + roleName + ' created by ' +
+      loadoutAuthor +
+      '";\n' +
+      '\t\tcomment "Note: This is a machine generated loadout file";\n\n'
+    loadoutContainer += "\t\t" + block1 + "\n\n"
+    loadoutContainer += "\t\t" + block2 + "\n\n"
+    loadoutContainer += "\t\t" + block3 + "\n\n"
+    loadoutContainer += "\t\t" + block4 + "\n\n"
+    loadoutContainer += "\t\t" + block5 + "\n\n"
+
+    loadoutContainer +=
+      '\t\thintSilent "Loadout Selected - Squad/Teamlead";\n'
+
+    return loadoutContainer
+  }
+
   generateCodeClickHandler() {
     const data = this.props.projectData
-    console.log("data: ", data)
+    let loadoutSqfCode = ""
 
-    fetch(sampleLoadoutUrl)
-      .then((sampleLoadout) => sampleLoadout.text())
-      .then((text) => {
-        // split code based on blocks of code
-        let loadout = text.split("\n\n")
+    // if side is selected, then add code to switch sides
+    if (data.side) {
+      let faction;
+      if (data.side === "West") faction = "west"
+      else if (data.side === "East") faction = "east"
+      else if (data.side === "Independent") faction = "resistance"
 
-        // if code is copied from google docs/ words then do this
-        if (loadout.length === 1) loadout = text.split("\r\n\r\n")
+      loadoutSqfCode += 'comment "Change player side to ' + data.side + '";\n'
+      loadoutSqfCode += 'private _player = player;\n'
+      loadoutSqfCode += '[player] join createGroup ' + faction + ';\n'
+      loadoutSqfCode += 'selectNoPlayer;\n'
+      loadoutSqfCode += 'selectPlayer _player;\n\n'
+    }
 
-        // index 0; loadout author comment
-        const block0 = loadout[0]
-        const loadoutAuthor = encodeURI(
-          block0
-            .replace(/(comment "Exported from Arsenal by )/g, "")
-            .replace(/(";)/g, "")
-        )
+    // add code for switch cases
+    loadoutSqfCode += 'comment "Select loadout";\n'
+    loadoutSqfCode += '_loadout = (_this select 3) select 0;\n'
+    loadoutSqfCode += 'switch (_loadout) do {\n'
 
-        // index 1; unit local; ignore this block
+    // add case for each loadout
+    let caseIndex = 0
+    for (const loadoutId in data.loadouts) {
+      const loadout = data.loadouts[loadoutId].code
+      const roleName = data.loadouts[loadoutId].name
 
-        // index 2; remove all gear
-        const block1 = this.addTabs(loadout[2]).replaceAll("this", "player")
+      loadoutSqfCode += '\tcase ' + caseIndex + ': {\n'
+      loadoutSqfCode += this.getProcessedCode(loadout, roleName)
+      loadoutSqfCode += '\t};\n\n'
 
-        // index 3; add weapons
-        const block2 = this.addTabs(loadout[3]).replaceAll("this", "player")
+      caseIndex++
+    }
 
-        // index 4; add containers
-        const block3 = this.addTabs(loadout[4]).replaceAll("this", "player")
+    // close switch case
+    loadoutSqfCode += '};'
 
-        // index 5; add items in containers
-        const block4 = this.addTabs(loadout[5]).replaceAll("this", "player")
-
-        // index 6; add items
-        const block5 = this.addTabs(loadout[6]).replaceAll("this", "player")
-
-        // index 7; set identity; ignore this block
-
-        // add all blocks to a loadout string
-        let loadoutContainer =
-          '\t\tcomment "Loadout for {RoleName} created by ' +
-          loadoutAuthor +
-          '";\n' +
-          '\t\tcomment "Note: This is a machine generated loadout file";\n\n'
-        loadoutContainer += "\t\t" + block1 + "\n\n"
-        loadoutContainer += "\t\t" + block2 + "\n\n"
-        loadoutContainer += "\t\t" + block3 + "\n\n"
-        loadoutContainer += "\t\t" + block4 + "\n\n"
-        loadoutContainer += "\t\t" + block5 + "\n\n"
-
-        loadoutContainer +=
-          '\t\thintSilent "Loadout Selected - Squad/Teamlead";'
-
-        this.setState({ code: JSON.stringify(loadout, undefined, 2) })
-        this.setState({ code: loadoutContainer })
-      })
+    this.setState({ code: loadoutSqfCode })
   }
 
   render() {
