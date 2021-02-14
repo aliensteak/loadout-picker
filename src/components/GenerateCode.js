be21a14ec0ebd4c4ae3a32309c4a4e8b8e73b103
Rohan Tejeswar <roh123098@gmail.com>
2021-02-14 18:12:36 +0530

    changed comment strucuture

--- src/components/GenerateCode.js
+++ src/components/GenerateCode.js
@@ -13,16 +13,16 @@ class GenerateCode extends Component {
   }
 
   addTabs(string) {
-    if (string.indexOf('\r') === -1)
-      string = string.replaceAll(';\n', ';\n\t\t')
-    else
-      string = string.replaceAll(';\r\n', ';\n\t\t')
+    if (string.indexOf("\r") === -1)
+      string = string.replaceAll(";\n", ";\n\t\t")
+    else string = string.replaceAll(";\r\n", ";\n\t\t")
 
     return string
   }
 
   generateCodeClickHandler() {
     const data = this.props.projectData
+    console.log("data: ", data)
 
     fetch(sampleLoadoutUrl)
       .then((sampleLoadout) => sampleLoadout.text())
@@ -44,34 +44,36 @@ class GenerateCode extends Component {
         // index 1; unit local; ignore this block
 
         // index 2; remove all gear
-        const block1 = this.addTabs(loadout[2])
+        const block1 = this.addTabs(loadout[2]).replaceAll("this", "player")
 
         // index 3; add weapons
-        const block2 = this.addTabs(loadout[3])
+        const block2 = this.addTabs(loadout[3]).replaceAll("this", "player")
 
         // index 4; add containers
-        const block3 = this.addTabs(loadout[4])
+        const block3 = this.addTabs(loadout[4]).replaceAll("this", "player")
 
         // index 5; add items in containers
-        const block4 = this.addTabs(loadout[5])
+        const block4 = this.addTabs(loadout[5]).replaceAll("this", "player")
 
         // index 6; add items
-        const block5 = this.addTabs(loadout[6])
+        const block5 = this.addTabs(loadout[6]).replaceAll("this", "player")
 
         // index 7; set identity; ignore this block
 
         // add all blocks to a loadout string
         let loadoutContainer =
-          '\t\tcomment "This is a machine generated loadout file. This loadout is created by ' +
+          '\t\tcomment "Loadout for {RoleName} created by ' +
           loadoutAuthor +
-          '";\n\n'
-        loadoutContainer += '\t\t' + block1 + '\n\n'
-        loadoutContainer += '\t\t' + block2 + '\n\n'
-        loadoutContainer += '\t\t' + block3 + '\n\n'
-        loadoutContainer += '\t\t' + block4 + '\n\n'
-        loadoutContainer += '\t\t' + block5 + '\n\n'
+          '";\n' +
+          '\t\tcomment "Note: This is a machine generated loadout file";\n\n'
+        loadoutContainer += "\t\t" + block1 + "\n\n"
+        loadoutContainer += "\t\t" + block2 + "\n\n"
+        loadoutContainer += "\t\t" + block3 + "\n\n"
+        loadoutContainer += "\t\t" + block4 + "\n\n"
+        loadoutContainer += "\t\t" + block5 + "\n\n"
 
-        loadoutContainer += '\t\thintSilent "Loadout Selected - Squad/Teamlead";'
+        loadoutContainer +=
+          '\t\thintSilent "Loadout Selected - Squad/Teamlead";'
 
         this.setState({ code: JSON.stringify(loadout, undefined, 2) })
         this.setState({ code: loadoutContainer })
