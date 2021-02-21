778182a98f64c6eb231130eb4f62121261de1eea
Rohan Tejeswar <roh123098@gmail.com>
2021-02-22 01:29:20 +0530

    created about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react"
+import loadoutPickerScreenshot from "./about/loadout-selector.jpg"
+import style from "./about/About.module.css"
 
-export default function About() {
+function About() {
   return (
-    <div>
-      
-    </div>
+    <section className='uk-container-large uk-padding-large' style={{ overflowX: "hidden", height: 'calc(100% - 60px)' }}>
+      <div className='uk-flex uk-flex-center uk-margin-top'>
+        <div className='uk-inline'>
+          <img
+            src={loadoutPickerScreenshot}
+            alt='context menu in-game screenshot'
+            className='uk-margin-auto'
+          />
+          <div class='uk-overlay uk-position-bottom' style={{ color: "white", backgroundColor: "rgba(0,0,0,0.5)" }}>
+            Loadouts appear on objects and can be selected via the context-menu
+          </div>
+        </div>
+      </div>
+
+      <div className="uk-card uk-card-default uk-width-expand uk-margin-top uk-margin-bottom" >
+        <div className="uk-card-body">
+          <div>
+            <h4>About</h4>
+            <hr/>
+            <p>
+              Loadout Picker is a tool allows you to compile your exported BI Arsenal loadout into an auto-generated sqf code for a <code>.sqf</code> file and <code>init</code> to let a player use ArmA's context-menu/scroll-wheel on any object and pick a pre-made loadout.
+            </p>
+          </div>
+          <br/>
+          <div>
+            <h4>Quirks</h4>
+            <hr/>
+            <p>
+              This tool lets you pick a faction when creating a script. It is useful when you have pre-made kits from multiple factions and want to play against opposing factions. The quirk is that it creates a new squad for each player who uses this script. So keep in mind that whenever a script generated from this tool is used, players will need to rejoin the squad or group.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
+
+export default About
